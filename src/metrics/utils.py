772e241a354aa795e421a91873c033cfaa6544d9
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2025-12-25 17:39:57 +0500

    added wer and cer

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -2,12 +2,38 @@
 
 # Don't forget to support cases when target_text == ''
 
+from Levenshtein import distance
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    """
+    Character Error Rate
+    """
+    if len(target_text) == 0:
+        return 1.0 if len(predicted_text) > 0 else 0.0
+    
+    # Calculate Levenshtein distance
+    edit_distance = distance(target_text, predicted_text)
+    # Calculate CER
+    cer = edit_distance / len(target_text)
+    return cer
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    """
+    Word Error Rate
+    """
+
+    target_words = target_text.split()
+    predicted_words = predicted_text.split()
+
+    # Если вдруг ошибка и нет слов в target_text
+    if len(target_words) == 0:
+        return 1.0
+    
+    substitutions = sum(1 for target, predicted in zip(target_words, predicted_words) if target != predicted)
+    deletions = len(target_words) - len(predicted_words)
+    insertions = len(predicted_words) - len(target_words)
+    total_words = len(target_words)
+
+    wer = (substitutions + deletions + insertions) / total_words
+    return wer
