25558ad1e75c0f18ede98d18c85721bb2570aabf
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2025-12-27 15:10:34 +0500

    added LMBeamWERMetric

--- src/metrics/wer.py
+++ src/metrics/wer.py
@@ -7,9 +7,6 @@ from src.metrics.base_metric import BaseMetric
 from src.metrics.utils import calc_wer
 from src.text_encoder import CTCTextEncoder
 
-
-
-# TODO beam search / LM versions
 # Note: they can be written in a pretty way
 # Note 2: overall metric design can be significantly improved
 
@@ -57,6 +54,39 @@ class BeamWERMetric(BaseMetric):
                 pred = self.text_encoder.ctc_beam_search(seq_log_probs, self.beam_size, length=seq_len)
             else:
                 argmax_pred = torch.argmax(seq_log_probs, dim=-1).numpy()
+                pred = self.text_encoder.ctc_decode(argmax_pred)
+            
+            target_norm = self.text_encoder.normalize_text(target)
+            wers.append(calc_wer(target_norm, pred))
+
+        return np.mean(wers)
+
+
+class LMBeamWERMetric(BaseMetric):
+    def __init__(self, text_encoder: CTCTextEncoder, beam_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+
+    def __call__(
+        self, logits: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ) -> float:
+        lengths = log_probs_length.detach().cpu().numpy().flatten()
+        logits = logits.detach().cpu()
+
+        wers = []
+        for idx, (seq_len, target) in enumerate(zip(lengths, text)):
+            seq_len = int(seq_len)
+            if seq_len <= 0:
+                wers.append(1.0)
+                continue
+
+            seq_logits = logits[idx, :seq_len]
+            
+            if hasattr(self.text_encoder, "lm_ctc_beam_search"):
+                pred = self.text_encoder.lm_ctc_beam_search(seq_logits, self.beam_size)
+            else:
+                argmax_pred = torch.argmax(seq_logits, dim=-1).numpy()
                 pred = self.text_encoder.ctc_decode(argmax_pred)
             
             target_norm = self.text_encoder.normalize_text(target)
--- src/text_encoder/ctc_text_encoder.py
+++ src/text_encoder/ctc_text_encoder.py
@@ -1,10 +1,13 @@
 import re
 from string import ascii_lowercase
 from collections import defaultdict
+import numpy as np
+from pyctcdecode import build_ctcdecoder
 
 import torch
 
-# TODO add BPE, LM, Beam Search support
+
+# TODO add BPE
 # Note: think about metrics and encoder
 # The design can be remarkably improved
 # to calculate stuff more efficiently and prettier
@@ -13,7 +16,14 @@ import torch
 class CTCTextEncoder:
     EMPTY_TOK = ""
 
-    def __init__(self, alphabet=None, **kwargs):
+    def __init__(
+            self,
+            alphabet=None,
+            lm_path: str = None,
+            unigrams_path: str = None,
+            *args,
+            **kwargs,
+    ):
         """
         Args:
             alphabet (list): alphabet for language. If None, it will be
@@ -28,6 +38,31 @@ class CTCTextEncoder:
 
         self.ind2char = dict(enumerate(self.vocab))
         self.char2ind = {v: k for k, v in self.ind2char.items()}
+
+
+        if lm_path is not None:
+            print(f"""\n LM path: {lm_path}""")
+
+            assert unigrams_path is not None, "LM and unigrams should be provided"
+
+            print(f"""\nUnigrams path: {unigrams_path}""")
+
+            unigrams = []
+            with open(unigrams_path, "r") as file:
+                for line in file:
+                    word = line.split()[0]
+                    unigrams.append(word)
+
+            self.lm_model = build_ctcdecoder(
+                labels=[self.EMPTY_TOK] + list(self.alphabet),
+                kenlm_model_path=lm_path,
+                unigrams=unigrams,
+                alpha=0.6,
+                beta=0.2,
+            )
+
+        else:
+            print("LM path is not provided, guess we're running without LM")
 
     def __len__(self):
         return len(self.vocab)
@@ -69,7 +104,7 @@ class CTCTextEncoder:
             return ""
         if len(inds) == 0:
             return ""
-        
+
         result = []
         prev = None
         for i in range(len(inds)):
@@ -82,29 +117,29 @@ class CTCTextEncoder:
             if idx != prev:
                 result.append(self.ind2char[idx])
             prev = idx
-        
+
         return "".join(result).strip()
 
-    def ctc_beam_search(self, log_probs, beam_size=10, length=None):
+    def ctc_beam_search(self, log_probs, beam_size: int = 10, length=None):
         """
         CTC beam search decoding.
         """
-        
+
         if length == 0:
             return ""
 
         EMPTY_TOK = '^'
-        
+
         probs = log_probs.exp()
-        
+
         beam = defaultdict(float)
         beam[("", EMPTY_TOK)] = 1.0
-        
+
         for t in range(length):
             proba = probs[t].cpu().numpy()
             beam = self._extend_and_merge_beam(beam, proba, EMPTY_TOK)
             beam = self._truncate(beam, beam_size)
-        
+
         best_key = max(beam, key=lambda x: beam[x])
         prefix, last_char = best_key
         if last_char != EMPTY_TOK:
@@ -120,7 +155,7 @@ class CTCTextEncoder:
         for (prefix, last_char), beam_prob in beam.items():
             for ind, pr in enumerate(proba):
                 new_char = self.ind2char[ind]
-                
+
                 if ind == 0:
                     # Blank token - don't add to prefix, reset last_char
                     new_beam[(prefix, empty_tok)] += beam_prob * pr
@@ -135,6 +170,9 @@ class CTCTextEncoder:
                         new_prefix = prefix + last_char
                     new_beam[(new_prefix, new_char)] += beam_prob * pr
         return new_beam
+
+    def lm_ctc_beam_search(self, logits: np.ndarray, beam_size: int = 20):
+        return self.lm_model.decode(logits=logits, beam_width=beam_size)
 
     @staticmethod
     def normalize_text(text: str):
