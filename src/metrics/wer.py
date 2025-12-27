3c7ef5c52e29589a22203623e28e72a051fb3564
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2025-12-27 14:54:23 +0500

    added BeamWERMetric

--- src/metrics/wer.py
+++ src/metrics/wer.py
@@ -1,10 +1,13 @@
 from typing import List
-
+import numpy as np
 import torch
 from torch import Tensor
 
 from src.metrics.base_metric import BaseMetric
 from src.metrics.utils import calc_wer
+from src.text_encoder import CTCTextEncoder
+
+
 
 # TODO beam search / LM versions
 # Note: they can be written in a pretty way
@@ -27,3 +30,36 @@ class ArgmaxWERMetric(BaseMetric):
             pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             wers.append(calc_wer(target_text, pred_text))
         return sum(wers) / len(wers)
+
+
+class BeamWERMetric(BaseMetric):
+    def __init__(self, text_encoder: CTCTextEncoder, beam_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ) -> float:
+        lengths = log_probs_length.detach().cpu().numpy().flatten()
+        log_probs = log_probs.detach().cpu()
+
+        wers = []
+        for idx, (seq_len, target) in enumerate(zip(lengths, text)):
+            seq_len = int(seq_len)
+            if seq_len <= 0:
+                wers.append(1.0)
+                continue
+
+            seq_log_probs = log_probs[idx, :seq_len]
+            
+            if hasattr(self.text_encoder, "ctc_beam_search"):
+                pred = self.text_encoder.ctc_beam_search(seq_log_probs, self.beam_size, length=seq_len)
+            else:
+                argmax_pred = torch.argmax(seq_log_probs, dim=-1).numpy()
+                pred = self.text_encoder.ctc_decode(argmax_pred)
+            
+            target_norm = self.text_encoder.normalize_text(target)
+            wers.append(calc_wer(target_norm, pred))
+
+        return np.mean(wers)
