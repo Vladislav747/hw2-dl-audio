4a6e44cf91e8db61e93c12bb014430deb06a0e3f
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2025-12-27 23:46:00 +0500

    added asr augmentations

--- src/transforms/wav_augs/__init__.py
+++ src/transforms/wav_augs/__init__.py
@@ -1,1 +1,7 @@
 from src.transforms.wav_augs.gain import Gain
+from src.transforms.wav_augs.pitch_shift import PitchShift
+from src.transforms.wav_augs.noise import Noise
+from src.transforms.wav_augs.time_stretch import TimeStretch
+from src.transforms.wav_augs.shift import Shift
+
+all = ["Gain", "Noise", "PitchShift", "TimeStretch", "Shift"]
